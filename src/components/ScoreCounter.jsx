89cac59d902b69820e56ddaa4dc37d08e04b1cc2
bharratgehlot <161546117+bharratgehlot@users.noreply.github.com>
2025-09-24 23:46:09 +0530

    fixes scorescounter

--- src/components/ScoreCounter.jsx
+++ src/components/ScoreCounter.jsx
@@ -16,16 +16,21 @@ function ScoreCounter({answers, questions}) {
         return total + question.marks;
       } else if (question.type === "MCQ" && answerData.answer && question.negativeMarks > 0) {
         console.log(`Subtracting ${question.negativeMarks} for MCQ Q${questionID}`);
-        return total - question.negativeMarks;
+        return Math.max (0, total - question.negativeMarks);
       }
       return total;
     }, 0); 
    
   },[answers, questions]);
 
+  const totalMarks = useMemo(() => {
+    return questions.reduce((total, question) => total + question.marks, 0);
+  }, [questions]);
+
+
   return (
     <div className={styles.scoreCounter}>
-      <h3>Score: {score}/100</h3>
+      <h3>Score: {Math.round(score * 100)/100}/{totalMarks}</h3>
     </div>
   )
 
